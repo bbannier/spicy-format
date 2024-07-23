19151061ccc5645f5e033370efd591bb7e369941
Benjamin Bannier <bbannier@gmail.com>
2024-07-23 15:14:57 +0200

    Migrate from `vergen` to `vergen-git2`

--- build.rs
+++ build.rs
@@ -3,9 +3,13 @@ use std::path::Path;
 fn main() {
     let is_git_repo = Path::new(".git").exists();
     if is_git_repo {
-        vergen::EmitBuilder::builder()
-            .idempotent()
-            .git_describe(true, true, None)
+        let git2 = vergen_git2::Git2Builder::default()
+            .describe(true, true, None)
+            .build()
+            .unwrap();
+        vergen_git2::Emitter::new()
+            .add_instructions(&git2)
+            .unwrap()
             .emit()
             .unwrap();
     }
