4fdc4f89db04707efcbf43db01c5e0851c91245f
Benjamin Bannier <bbannier@gmail.com>
2025-07-09 10:52:48 +0200

    Bump `tree-sitter-spicy`
    This bump contains parsing of comment bodies. We need an extra twist for
    that since `##` could be parsed as a `(comment (comment))`.

--- src/query.scm
+++ src/query.scm
@@ -297,7 +297,12 @@
 
 (visibility) @append_space
 
-(comment) @prepend_input_softline @append_input_softline @append_hardline
+(comment
+) @prepend_input_softline @append_input_softline @append_hardline
+; `comment` is an `extra` node so it can appear anywhere, even inside a
+; `comment_body`. Avoid formatting at all if we have a "nested comment"
+; like in `##`.
+(comment (comment)) @leaf
 
 (attribute) @prepend_space
 (attribute (_) @append_antispace "=" @append_antispace (_))
