79e60a7affeda8194a5806ad3ed8374e8eeeedae
Benjamin Bannier <bbannier@gmail.com>
2025-11-14 17:47:39 +0100

    Move most CLI tests to `try_cmd`

--- README.md
+++ README.md
@@ -113,24 +113,6 @@ tree-sitter-spicy = { path = "path/to/local/version" }
 
 ### Tests
 
-Test cases are collected and automatically discovered by the test harness in
-the [`corpus`/](corpus/) directory. To execute the test harness run
-
-```console
-cargo t
-```
-
-For each `corpus/<INPUT>.spicy` the repository contains a matching expected
-result baseline `corpus/<INPUT>.spicy.expected`. To update the baselines
-execute the test harness with the environment variable `UPDATE_BASELINE` set.
-
-```console
-UPDATE_BASELINE=1 cargo t
-```
-
-This will update or if needed create the outdated baselines. When adding new
-tests to the corpus, commit the baseline as well.
-
 To run formatting against an external corpus run the test suite with
 `SPICY_FORMAT_EXTERNAL_CORPUS` set.
 
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,5 @@
 use std::{
     collections::HashMap,
-    io::{Read, Write},
     path::{Path, PathBuf},
     process::{Command, Output, Stdio},
 };
@@ -12,39 +11,6 @@ use rayon::prelude::*;
 use tempfile::NamedTempFile;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-#[test]
-fn trailing_newline_stdin() -> Result<()> {
-    let mut child = Command::new(cargo::cargo_bin!())
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
-
-    let mut stdin = child.stdin.as_ref().unwrap();
-    stdin.write("1;\n".as_bytes())?;
-
-    child.wait()?;
-    let mut stdout = String::new();
-    child.stdout.unwrap().read_to_string(&mut stdout)?;
-    assert_eq!(stdout, "1;\n");
-    Ok(())
-}
-
-#[test]
-fn trailing_newline_file() -> Result<()> {
-    let mut input = NamedTempFile::new()?;
-    input.write("1;\n".as_bytes())?;
-
-    let cmd = Command::new(cargo::cargo_bin!())
-        .arg(input.path())
-        .stdout(Stdio::piped())
-        .output()?;
-
-    let stdout = String::from_utf8(cmd.stdout)?;
-
-    assert_eq!(stdout, "1;\n");
-    Ok(())
-}
 
 #[test]
 fn do_not_touch_unmodified() -> Result<()> {
@@ -64,7 +30,6 @@ fn do_not_touch_unmodified() -> Result<()> {
 
     Ok(())
 }
-
 /// Helper to format an input file via the CLI.
 fn format<P>(path: P) -> Result<String>
 where
@@ -81,76 +46,6 @@ where
     let output = String::from_utf8(output.stdout)?;
 
     Ok(output)
-}
-
-#[test]
-fn corpus() -> Result<()> {
-    use pretty_assertions::assert_eq;
-
-    let corpus = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("corpus");
-
-    let update_baseline = std::env::var("UPDATE_BASELINE").is_ok();
-
-    let files = walkdir::WalkDir::new(&corpus)
-        .into_iter()
-        .filter_map(|e| {
-            let e = e.ok()?;
-
-            if e.file_type().is_file()
-                && e.path().extension().and_then(|ext| ext.to_str()) == Some("spicy")
-            {
-                Some(e)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    files
-        .par_iter()
-        .filter_map(|t| {
-            let input = t.path();
-
-            let output = {
-                let path = input.to_path_buf();
-
-                let file_name = format!(
-                    "{}.expected",
-                    path.file_name()
-                        .unwrap_or_else(|| panic!(
-                            "cannot get filename component of {}",
-                            path.display()
-                        ))
-                        .to_string_lossy()
-                );
-
-                let mut o = path;
-                assert!(o.pop());
-
-                o.join(file_name)
-            };
-
-            let formatted = format(&input).unwrap_or_else(|_| {
-                panic!("cannot format source file {}", t.path().to_string_lossy())
-            });
-
-            if !update_baseline {
-                let expected = std::fs::read_to_string(output).expect("cannot read baseline");
-                assert_eq!(
-                    expected,
-                    formatted,
-                    "while formatting {}",
-                    t.path().display()
-                );
-            } else {
-                std::fs::write(output, formatted).expect("cannot update baseline");
-            }
-
-            Some(1)
-        })
-        .collect::<Vec<_>>();
-
-    Ok(())
 }
 
 #[test]
@@ -224,4 +119,11 @@ fn corpus_external() -> miette::Result<()> {
             failures.join("\n")
         ))
     }
+}
+
+#[test]
+fn cli_tests() {
+    trycmd::TestCases::new()
+        .case("tests/cmd/*.toml")
+        .default_bin_name("spicy-format");
 }
